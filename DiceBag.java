4475b18cb85365cd26cea21317c88e3b10be6aca
ms1646 <ms1646@wildcats.unh.edu>
2020-10-11 23:42:38 -0400

    Home Work 6

---
+++ DiceBag.java
@@ -0,0 +1,39 @@
+package hw6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+// This class holds the dice(or tokens) to be drawn.
+//
+public class DiceBag {
+	
+	private List<Token> bag = new ArrayList<Token>();
+	
+	public boolean addToken(Token t) {
+		bag.add(t);
+		return true;
+	}
+
+	public Token drawToken() {
+		int upperRange = bag.size();
+		Token t = null;
+		
+		if (upperRange > 1 ) {
+			int die = ThreadLocalRandom.current().nextInt(1, upperRange);
+			t = bag.remove(die);
+		}   
+
+		return t;
+	}
+	
+	public String toString() {
+		String ret = " Dice left: " + bag.size() + "\n";
+		
+		for (Token t:bag) {
+			ret += t.toString() + "\n";
+		}
+
+		return ret;
+	}
+}
